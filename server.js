6591c4d5a5857a7cd9f0028d71d9e88b4fcd746e
chirstiankaikai <ckaikai19@gmail.com>
2022-07-20 21:17:18 -0400

    added get routes

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
-
 const express = require('express');
 const app = express();
 
-const allNotes = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
+
+const notes = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
-app.listen(PORT, () => {
+
+
+
+
+
+
+
+app.listen(3001, () => {
     console.log(`API server now on port ${PORT}!`);
 });
